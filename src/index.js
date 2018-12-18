aa85852eaa8867f0e208585cf3d3e57615b2cdda
Kevin Boyle <papershack@gmail.com>
2018-12-18 18:19:05 -0500

    add constructor & state

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude})
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+    }
     
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), 
-            (err) => console.log(err)
-        );
-        return <div>Latitude:</div>
+
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
