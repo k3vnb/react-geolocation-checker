a00d8427f61df749679dede873901e266f47c39b
Kevin Boyle <papershack@gmail.com>
2018-12-21 15:21:12 -0800

    refactor conditional rendering, update README

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const seasonConfig = {};
+const seasonConfig = {
+    summer: {
+        text: "Lext's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "burr",
+        iconName: 'snowflake'
+    }
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9){
@@ -12,13 +21,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burrr' : 'Lets hit the beach'
-    const icon = season === 'winter' ? 'snowflake' : 'sun'
+    const { text, iconName } = seasonConfig[season]; 
     return (
         <div>
-            <i className={`${icon}`} />
+            <i className={`${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${icon}`} />
+            <i className={`${iconName} icon`} />
         </div>);
 };
 
