bf4c95a8c3ab89ac88eb6dcc436fe2975479d3b4
Kevin Boyle <papershack@gmail.com>
2018-12-21 15:13:26 -0800

    add preliminary conditional rendering

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+const seasonConfig = {};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+       return lat > 0 ? 'summer' : 'winter'; 
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burrr' : 'Lets hit the beach'
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
+    return (
+        <div>
+            <i className={`${icon}`} />
+            <h1>{text}</h1>
+            <i className={`${icon}`} />
+        </div>);
 };
 
 export default SeasonDisplay;
