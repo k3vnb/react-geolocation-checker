2353389412af0560a56026f922244a0b7e4f1fb0
Kevin Boyle <papershack@gmail.com>
2018-12-21 16:11:01 -0800

    add spinner component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,7 @@
 import './SeasonDisplay.css';
 import React from 'react';
 
+
 const seasonConfig = {
     summer: {
         text: "Lext's hit the beach",
@@ -23,6 +24,7 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const { text, iconName } = seasonConfig[season]; 
+    
     return (
         <div className={`season-display ${season}`}>
             <i className={`icon-left massive ${iconName} icon`} />
